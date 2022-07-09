12064c97787fdd50686200d6e9b183b7d9f9c150
Louis Stowasser <louisstow@gmail.com>
2022-07-09 10:14:33 +1000

    * Add XML mode to cheerio middleware

--- lib/middleware/CheerioMiddleware.d.ts
+++ lib/middleware/CheerioMiddleware.d.ts
@@ -1,6 +1,10 @@
 import { Middleware } from "../Middleware";
 import { QueueItem } from "../Queue";
 declare class CheerioMiddleware extends Middleware {
+    isXML: boolean;
+    constructor(options: {
+        isXML: boolean;
+    });
     processResponse(item: QueueItem): Promise<boolean>;
 }
 export { CheerioMiddleware };
--- src/middleware/CheerioMiddleware.ts
+++ src/middleware/CheerioMiddleware.ts
@@ -4,10 +4,19 @@ import { QueueItem } from "../Queue";
 import cheerio from "cheerio";
 
 class CheerioMiddleware extends Middleware {
+  isXML: boolean = false;
+
+  constructor(options: { isXML: boolean }) {
+    super();
+    this.isXML = options.isXML || false;
+  }
+
   processResponse(item: QueueItem) {
     const resp = item.request.response;
     if (resp) {
-      resp.data = cheerio.load(resp.raw);
+      resp.data = cheerio.load(resp.raw, {
+        xmlMode: this.isXML,
+      });
     }
 
     return Promise.resolve(true);
